1ff62e13299575c7a03a37c6f1666a2b308e61cd
ZaL <z4ls@live.com>
2015-02-18 22:21:06 +0300

    tweak cars

--- resources/views/module/cars/_result.blade.php
+++ resources/views/module/cars/_result.blade.php
@@ -4,33 +4,31 @@
             <!-- Drop Down Filter-->
             <div class="row sort-row">
                 <h3 class="results-title pull-left clearfix">Results</h3>
+
                 <div class="dropdown">
                     <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
                         Show
                         <span class="caret"></span>
                     </button>
                     <ul class="dropdown-menu" ng-model="sortorder" role="menu" aria-labelledby="dropdownMenu1">
-                        <li role="presentation">
-                            <a role="menuitem" tabindex="-1" href="#" value="+created_at">Year: New to Old</a></li>
-                        <li role="presentation">
-                            <a role="menuitem" tabindex="-1" href="#" value="-year">Year: Old to new</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Price: Low to High</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Price: High to Low </a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Mileage: High to Low</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Mileage: Low to High</a></li>
-                        <li role="presentation">
-                            <a role="menuitem" tabindex="-1" href="#" selected value="-created_at">Submition: New to Old</a>
+                        <li>
+                            <option value="+created_at">Oldest</option>
                         </li>
-                        <li role="presentation">
-                            <a role="menuitem" tabindex="-1" href="#" selected value="+created_at">Submition: Old to New</a>
+                        <li><a href="#" value="-year">Year: Old to new</a></li>
+                        <li>
+                            <option value="+price">Low Price</option>
                         </li>
-
+                        <li><a href="#">Price: High to Low </a></li>
+                        <li><a href="#">Mileage: High to Low</a></li>
+                        <li><a href="#">Mileage: Low to High</a></li>
+                        <li><a href="#">Submition: New to Old</a></li>
+                        <li><a href="#" value="+created_at">Submition: Old to New</a></li>
                     </ul>
                 </div>
 
                 {{--<div class="btn-group pull-left clearfix">--}}
-                {{--<button class="btn btn-default dropdown-toggle toggle-reset" data-toggle="dropdown">All <span--}}
-                {{--class="caret"></span></button>--}}
+                {{--<button class="btn btn-default dropdown-toggle toggle-reset" data-toggle="dropdown">All--}}
+                {{--<span class="caret"></span></button>--}}
                 {{--<ul class="dropdown-menu" ng-model="sortorder" class="input-small">--}}
                 {{--<option value="+created_at">Oldest</option>--}}
                 {{--<option value="+year">Lower Year</option>--}}
