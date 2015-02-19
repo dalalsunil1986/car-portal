ec0ce682dc3dff1b92f14426ca633417a43a360a
ZaL <z4ls@live.com>
2015-02-19 22:26:35 +0300

    adjust messaging

--- public/app/controllers/CarsViewController.js
+++
@@ -1,6 +0,0 @@
-app.controller('CarsViewController', function ($scope, $stateParams, CarService) {
-    $scope.car = {};
-    CarService.getView($stateParams.id).then(function (data) {
-        $scope.car = data;
-    });
-});
