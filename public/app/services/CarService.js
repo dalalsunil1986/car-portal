c4e8a28899e70914a37a3eeeb94df881faa96809
ZaL <z4ls@live.com>
2015-02-20 11:47:03 +0300

    fix car service page

--- public/app/services/CarService.js
+++ public/app/services/CarService.js
@@ -14,89 +14,60 @@ function CarService($http, $q) {
     };
 
     function getFilter(filters) {
-        var defer = $q.defer();
-        var params = {
-            make: filters.selectedMakes,
-            brand: filters.selectedBrands,
-            type: filters.selectedTypes,
-            model: filters.selectedModels
-        };
-        $http({
-            url: 'api/cars/filter',
-            method: "GET",
-            params: params
-        }).success(function (data) {
-                defer.resolve(data);
+        var deferred = $q.defer();
+        $http.get('api/cars/filter?make=' + filters.selectedMakes + '&brand=' + filters.selectedBrands + '&type=' + filters.selectedTypes + '&model=' + filters.selectedModels)
+            .success(function (data) {
+                deferred.resolve(data);
             }
         ).error(function () {
-                defer.reject('An error has occurred ');
+                deferred.reject('An error has occurred ');
             }
         );
-        return defer.promise;
+        return deferred.promise;
     }
 
     function getFilterNames(filters) {
-        var defer = $q.defer();
-        var params = {
-            make: filters.selectedMakes,
-            brand: filters.selectedBrands,
-            type: filters.selectedTypes,
-            model: filters.selectedModels
-        };
-        $http({
-            url: 'api/cars/name',
-            method: "GET",
-            params: params
-        }).success(function (data) {
-                defer.resolve(data);
+        var deferred = $q.defer();
+
+        $http.get('api/cars/name?make=' + filters.selectedMakes + '&brand=' + filters.selectedBrands + '&type=' + filters.selectedTypes + '&model=' + filters.selectedModels)
+            .success(function (data) {
+                deferred.resolve(data);
             }
         ).error(function () {
-                defer.reject('An error has occurred ');
+                deferred.reject('An error has occurred ');
             }
         );
-        return defer.promise;
+        return deferred.promise;
     }
 
     function getIndex(filters) {
-        var defer = $q.defer();
-        var params = {
-            make: filters.selectedMakes,
-            brand: filters.selectedBrands,
-            type: filters.selectedTypes,
-            model: filters.selectedModels,
-            price_from: filters.priceFrom,
-            price_to: filters.priceTo,
-            mileage_from: filters.mileageFrom,
-            mileage_to: filters.mileageTo,
-            year_from: filters.yearFrom,
-            year_to: filters.yearTo,
-            page: filters.page
-        };
-        $http({
-            url: 'api/cars',
-            method: "GET",
-            params: params
-        }).success(function (data) {
-                defer.resolve(data);
+        var deferred = $q.defer();
+
+        $http.get('api/cars?make=' + filters.selectedMakes + '&brand=' + filters.selectedBrands + '&type=' + filters.selectedTypes + '&model=' + filters.selectedModels +
+            '&price_from=' + filters.priceFrom + '&price_to=' + filters.priceTo + '&mileage_from=' + filters.mileageFrom + '&mileage_to=' + filters.mileageTo +
+            '&year_from=' + filters.yearFrom + '&year_to=' + filters.yearTo + '&page=' + filters.page
+        )
+            .success(function (data) {
+                deferred.resolve(data);
             }
         ).error(function () {
-                defer.reject('An error has occurred ');
+                deferred.reject('An error has occurred ');
             }
         );
-        return defer.promise;
+        return deferred.promise;
     }
 
     function getView(id) {
-        var defer = $q.defer();
+        var deferred = $q.defer();
         $http.get('/api/cars/' + id)
             .success(function (data) {
-                defer.resolve(data);
+                deferred.resolve(data);
             }
         ).error(function () {
-                defer.reject('An error has occurred :(');
+                deferred.reject('An error has occurred :(');
             }
         );
-        return defer.promise;
+        return deferred.promise;
     }
 
 
