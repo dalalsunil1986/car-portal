ad03e4dc6824ba13526dc8ac5f6527be16eea742
ZaL <z4ls@live.com>
2015-02-21 15:48:36 +0300

    check auth in composer

--- app/Http/Composers/MessageComposer.php
+++ app/Http/Composers/MessageComposer.php
@@ -7,12 +7,16 @@ class MessageComposer {
 
     /**
      * @param View $view
+     * @return $this
      */
     public function compose(View $view)
     {
+        if ( !Auth::check() ) {
+            return $view->with('newMessagesCount', 0);
+        }
+
         $user          = Auth::user();
         $messagesCount = $user->newMessagesCount();
-
-        $view->with('newMessagesCount', $messagesCount);
+        return $view->with('newMessagesCount', $messagesCount);
     }
 }
