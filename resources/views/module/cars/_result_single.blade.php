e3666ec5390c2373c661298213fcfbfa73e15dd2
dakort <vincenzohx@yahoo.com>
2015-03-07 13:27:47 +0300

    Fixed inbox view style

--- resources/views/module/cars/_result_single.blade.php
+++ resources/views/module/cars/_result_single.blade.php
@@ -1,15 +1,16 @@
 
         <div class="row item-row post-item">
 
-            <div class="col-md-8 column title-color">
+
+            <div class="col-xs-10 col-sm-8 column title-color">
                 {!! $car->thumbnail ? '<a href="/cars/'. $car->id .'"><img src="/uploads/thumbnail/'.$car->thumbnail->name.'" class="img-responsive post-image"/></a>' : '<a href="/cars/'.$car->id.'"><img src="assets/img/custom/car-thumb.jpg" class="post-image"/></a>' !!}
 
                 <h3 class="post-title">
-                    {{ $car->year}} - {{ $car->model->brand->name }} - {{ $car->model->name}}
+                    <a href="{{ action('CarsController@show',$car->id) }}" >  {{ $car->year}} - {{ $car->model->brand->name }} - {{ $car->model->name}}  </a>
                 </h3>
             </div>
-            <div class="col-md-2 column cost-color">
-                <div class="post-option"><a href="{{ action('CarsController@show',$car->id) }}">View</a></div>
+            <div class="col-xs-2 col-sm-2 column cost-color">
+
                 <div class="post-option"><a href="{{ action('CarsController@edit',$car->id) }}">Edit</a></div>
 
                 <button type="button" class="delete delete-button" aria-hidden="true">
