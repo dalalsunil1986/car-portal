71dac6cc878708fa9f7526ecd3fd1d5fcc5af0e7
ZaL <z4ls@live.com>
2015-03-20 10:46:01 +0300

    remove filter debounce

--- resources/views/module/cars/_filter.blade.php
+++ resources/views/module/cars/_filter.blade.php
@@ -8,7 +8,7 @@
         <div class="form-group row">
             <label class="col-xs-3 padding-left">Make</label>
 
-            <ui-select multiple ng-model="filters.selectedMakes" ng-model-options="{ debounce: 3000 }" theme="select2" class="col-sm-8 col-xs-9 ">
+            <ui-select multiple ng-model="filters.selectedMakes" theme="select2" class="col-sm-8 col-xs-9 ">
                 <ui-select-match placeholder="Select a Make...">{[ $item.name ]}</ui-select-match>
                 <ui-select-choices repeat="make.id as make in makes | propsFilter: {name: $select.search} ">
                     <div ng-bind-html="make.name | highlight: $select.search"></div>
@@ -20,7 +20,7 @@
         <!--Brand Filter Start-->
         <div class="form-group row fit-screen">
             <label class="col-xs-3 padding-left">Brand</label>
-            <ui-select multiple ng-model="filters.selectedBrands " ng-model-options="{ debounce: 3000 }" theme="select2" class="col-sm-8 col-xs-9">
+            <ui-select multiple ng-model="filters.selectedBrands " theme="select2" class="col-sm-8 col-xs-9">
                 <ui-select-match placeholder="Select a Brand...">{[ $item.name ]}</ui-select-match>
                 <ui-select-choices repeat="brand.id as brand in brands | propsFilter: {name: $select.search}">
                     <div ng-bind-html="brand.name | highlight: $select.search"></div>
@@ -32,7 +32,7 @@
         <!--Type Filter Start-->
         <div class="form-group row fit-screen">
             <label class="col-xs-3 padding-left">Type</label>
-            <ui-select multiple ng-model="filters.selectedTypes" ng-model-options="{ debounce: 3000 }" theme="select2" class="col-sm-8 col-xs-9">
+            <ui-select multiple ng-model="filters.selectedTypes" theme="select2" class="col-sm-8 col-xs-9">
                 <ui-select-match placeholder="Select a Type...">{[ $item.name ]}</ui-select-match>
                 <ui-select-choices repeat="type.id as type in types  | propsFilter: {name: $select.search}">
                     <div ng-bind-html="type.name | highlight: $select.search"></div>
@@ -44,7 +44,7 @@
         <!--Model Filter Start-->
         <div class="form-group row">
             <label class="col-xs-3 padding-left">Model</label>
-            <ui-select multiple ng-model="filters.selectedModels" ng-model-options="{ debounce: 3000 }" ng-disabled="disabled" theme="select2" class="col-sm-8 col-xs-9">
+            <ui-select multiple ng-model="filters.selectedModels" ng-disabled="disabled" theme="select2" class="col-sm-8 col-xs-9">
                 <ui-select-match placeholder="Select a Model...">{[ $item.name ]}</ui-select-match>
                 <ui-select-choices repeat="model.id as model in models  | propsFilter: {name: $select.search}">
                     <div ng-bind-html="model.name | highlight: $select.search"></div>
@@ -55,17 +55,17 @@
 
         <div class="form-group row">
             <div class="range-div form-control-inline col-sm-11 col-xs-12" />
-                <input x-year-slider type="text" ng-model="yearslider" ng-model-options="{ debounce: 3000 }"/>
+                <input x-year-slider type="text" ng-model="yearslider" />
             </div>
         </div>
         <div class="form-group row">
             <div class="range-div form-control-inline col-sm-11  col-xs-12">
-                <input x-mileage-slider ng-model="mileageslider" filters="filters" type="text" ng-model-options="{ debounce: 3000 }"/>
+                <input x-mileage-slider ng-model="mileageslider" filters="filters" type="text" />
             </div>
         </div>
         <div class="form-group row">
             <div class="range-div form-control-inline col-sm-11 col-xs-12">
-                <input x-price-slider type="text" ng-model="priceslider" ng-model-options="{ debounce: 3000 }"/>
+                <input x-price-slider type="text" ng-model="priceslider"/>
             </div>
         </div>
 
